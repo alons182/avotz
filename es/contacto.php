9ddc68d0f8815c7199ecbbe7d4185ce3f6104704
alons182 <alons182@hotmail.com>
2013-09-19 13:29:03 -0600

    a

--- es/contacto.php
+++ es/contacto.php
@@ -38,7 +38,7 @@ if(isset($_POST['submitted'])) {
   // upon no failure errors let's email now!
   if(!isset($hasError)) {
     
-    $emailTo = 'alonso@avotz.com,alons182@hotmail.com';
+    $emailTo = 'info@avotz.com';
     $subject = 'Sitio Avotz - Submitted message from '.$name;
     $sendCopy = trim($_POST['sendCopy']);
     $body = "Name: $name \n\nEmail: $email \n\nComments: $comments";
