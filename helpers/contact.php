b359db287cd3fb47ac9e1be0407c93196211199a
alonso <alons182@hotmail.com>
2014-04-09 20:41:13 -0600

    analitics

--- helpers/contact.php
+++ helpers/contact.php
@@ -19,10 +19,10 @@ if(trim($_POST['comments']) === '') {
 }
 
 if($result=='ok'){
-	$emailTo = 'alonso@avotz.com';
+	$emailTo = 'info@avotz.com, oporto@avotz.com,alonso@avotz.com';
     $subject = 'Desde el formulario de contactenos del Sitio Avotz - Submitted message from '.$name;
     $body = "Nombre: $name \n\nEmail: $email \n\nComments: $comments";
-    $headers = 'From: ' .' <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $email;
+    $headers = 'From: ' .' <info@avotz.com>' . "\r\n" . 'Reply-To: ' . $email;
 
     mail($emailTo, $subject, $body, $headers);
         
